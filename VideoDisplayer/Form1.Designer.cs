b82e6ebf3706058dd91d95a725a516ee51fb4b09
Aaser Truusa <aasertruusa@gmail.com>
2020-04-21 00:37:02 +0300

    First Commit

---
+++ VideoDisplayer/Form1.Designer.cs
@@ -0,0 +1,93 @@
+﻿namespace VideoDisplayer
+{
+    partial class VideoDisplayer
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(VideoDisplayer));
+            this.MediaPlayer = new AxWMPLib.AxWindowsMediaPlayer();
+            this.videoURLs = new System.Windows.Forms.ListBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            ((System.ComponentModel.ISupportInitialize)(this.MediaPlayer)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // MediaPlayer
+            // 
+            this.MediaPlayer.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.MediaPlayer.Enabled = true;
+            this.MediaPlayer.Location = new System.Drawing.Point(176, 11);
+            this.MediaPlayer.Name = "MediaPlayer";
+            this.MediaPlayer.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("MediaPlayer.OcxState")));
+            this.MediaPlayer.Size = new System.Drawing.Size(480, 329);
+            this.MediaPlayer.TabIndex = 0;
+            this.MediaPlayer.Enter += new System.EventHandler(this.MediaPlayer_Enter);
+            // 
+            // videoURLs
+            // 
+            this.videoURLs.AllowDrop = true;
+            this.videoURLs.FormattingEnabled = true;
+            this.videoURLs.Location = new System.Drawing.Point(12, 11);
+            this.videoURLs.Name = "videoURLs";
+            this.videoURLs.Size = new System.Drawing.Size(156, 329);
+            this.videoURLs.TabIndex = 1;
+            this.videoURLs.SelectedIndexChanged += new System.EventHandler(this.videoURL_1_SelectedIndexChanged);
+            this.videoURLs.DragDrop += new System.Windows.Forms.DragEventHandler(this.videoURL_1_DragDrop);
+            this.videoURLs.DragEnter += new System.Windows.Forms.DragEventHandler(this.videoURL_1_DragEnter);
+            this.videoURLs.DragOver += new System.Windows.Forms.DragEventHandler(this.videoURLs_DragOver);
+            this.videoURLs.MouseDown += new System.Windows.Forms.MouseEventHandler(this.videoURLs_MouseDown);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.Multiselect = true;
+            // 
+            // VideoDisplayer
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(668, 352);
+            this.Controls.Add(this.videoURLs);
+            this.Controls.Add(this.MediaPlayer);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "VideoDisplayer";
+            this.Text = "Video Displayer";
+            ((System.ComponentModel.ISupportInitialize)(this.MediaPlayer)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private AxWMPLib.AxWindowsMediaPlayer MediaPlayer;
+        private System.Windows.Forms.ListBox videoURLs;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+    }
+}
+
---
+++ VideoDisplayer/Form1.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Drawing;
+using System.IO;
+using System.Windows.Forms;
+
+namespace VideoDisplayer
+{
+    public partial class VideoDisplayer : Form
+    {
+        public ListBox videoDIRs = new ListBox();
+        public VideoDisplayer()
+        {
+            InitializeComponent();
+            MediaPlayer.URL = null;
+        }
+
+        private void videoURL_1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            //MediaPlayer.URL = videoURLs.SelectedItem.ToString();
+            MediaPlayer.URL = videoDIRs.Items[videoURLs.SelectedIndex].ToString();
+            MediaPlayer.Ctlcontrols.stop();
+        }
+
+        private void videoURL_1_DragEnter(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+                e.Effect = DragDropEffects.All;
+            else
+                e.Effect = DragDropEffects.None;
+        }
+
+        private void videoURL_1_DragDrop(object sender, DragEventArgs e)
+        {
+            string[] s = (string[])e.Data.GetData(DataFormats.FileDrop, false);
+            int i;
+            for (i = 0; i < s.Length; i++)
+            {
+                videoURLs.Items.Add(Path.GetFileName(s[i]));
+                videoDIRs.Items.Add(s[i]);
+            }
+                
+        }
+
+        private void MediaPlayer_Enter(object sender, EventArgs e)
+        {
+            MediaPlayer.Ctlcontrols.play();
+        }
+
+        private void videoURLs_MouseDown(object sender, MouseEventArgs e)
+        {
+
+        }
+
+        private void videoURLs_DragOver(object sender, DragEventArgs e)
+        {
+
+        }
+
+    }
+
+    public class DragItem
+    {
+        public ListBox Client;
+        public int Index;
+        public object Item;
+
+        public DragItem(ListBox client, int index, object item)
+        {
+            Client = client;
+            Index = index;
+            Item = item;
+        }
+    }
+}
